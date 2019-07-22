350f14346a986886d560d79bef6c3987f887829d
Dallas-Norton <dallas.norton@domo.com>
2019-07-21 22:13:00 -0600

    better styling

--- src/Components/SummaryView.jsx
+++ src/Components/SummaryView.jsx
@@ -19,7 +19,11 @@ const useStyles = makeStyles(theme => ({
     flexGrow: '1',
     alignItems: 'baseline',
     justifyContent: 'space-between',
+    flexDirection: 'column'
   },
+  spacing: {
+    paddingBottom: theme.spacing(1),
+  }
 }));
 
 export default function SummaryView({property, onChange}) {
@@ -27,67 +31,77 @@ export default function SummaryView({property, onChange}) {
 
   return (
     <div className={classes.summaryContainer}>
-      <Typography>
-        <Link href={property.mlsURL} target="_blank">
-          {property.fullAddress}
-        </Link>
-      </Typography>
-      <FormControl>
-        <InputLabel htmlFor="adornment-listPrice">List Price</InputLabel>
-        <Input
-          type={'number'}
-          id="adornment-listPrice"
-          value={property.listPrice}
-          onChange={event =>
-            onChange(event.target.value, 'listPrice', property)
-          }
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
-        />
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="adornment-totalRent">Total Rent</InputLabel>
-        <Input
-          type={'number'}
-          id="adornment-totalRent"
-          value={property.totalRent}
-          onChange={event =>
-            onChange(event.target.value, 'totalRent', property)
-          }
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
-        />
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="adornment-cocReturn">
-          Estimated Rehab Cost
-        </InputLabel>
-        <Input
-          type={'number'}
-          id="adornment-estimatedRehabCost"
-          value={property.estimatedRehabCost}
-          onChange={event =>
-            onChange(event.target.value, 'estimatedRehabCost', property)
-          }
-          startAdornment={<InputAdornment position="start">$</InputAdornment>}
-        />
-      </FormControl>
-      <FormControl>
-        <InputLabel htmlFor="adornment-annualCOCReturn">
-          Cash on Cash Return
-        </InputLabel>
-        <Input
-          type={'number'}
-          id="adornment-annualCOCReturn"
-          value={property.annualCOCReturn}
-          onChange={event =>
-            onChange(event.target.value, 'annualCOCReturn', property)
-          }
-          endAdornment={<InputAdornment position="end">%</InputAdornment>}
-          aria-describedby="annualCOCReturn"
-          inputProps={{
-            'aria-label': 'Annual COC Returns',
-          }}
-        />
-      </FormControl>
+      <div className={classes.spacing}>
+        <Typography variant={"h5"}>
+          <Link href={property.mlsURL} target="_blank">
+            {property.fullAddress}
+          </Link>
+        </Typography>
+      </div>
+      <div>
+        <FormControl>
+          <InputLabel htmlFor="adornment-listPrice">List Price</InputLabel>
+          <Input
+            type={'number'}
+            id="adornment-listPrice"
+            value={property.listPrice}
+            onChange={event =>
+              onChange(event.target.value, 'listPrice', property)
+            }
+            startAdornment={
+              <InputAdornment position="start">$</InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="adornment-totalRent">Total Rent</InputLabel>
+          <Input
+            type={'number'}
+            id="adornment-totalRent"
+            value={property.totalRent}
+            onChange={event =>
+              onChange(event.target.value, 'totalRent', property)
+            }
+            startAdornment={
+              <InputAdornment position="start">$</InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="adornment-cocReturn">
+            Estimated Rehab Cost
+          </InputLabel>
+          <Input
+            type={'number'}
+            id="adornment-estimatedRehabCost"
+            value={property.estimatedRehabCost}
+            onChange={event =>
+              onChange(event.target.value, 'estimatedRehabCost', property)
+            }
+            startAdornment={
+              <InputAdornment position="start">$</InputAdornment>
+            }
+          />
+        </FormControl>
+        <FormControl>
+          <InputLabel htmlFor="adornment-annualCOCReturn">
+            Cash on Cash Return
+          </InputLabel>
+          <Input
+            type={'number'}
+            id="adornment-annualCOCReturn"
+            value={property.annualCOCReturn}
+            onChange={event =>
+              onChange(event.target.value, 'annualCOCReturn', property)
+            }
+            endAdornment={<InputAdornment position="end">%</InputAdornment>}
+            aria-describedby="annualCOCReturn"
+            inputProps={{
+              'aria-label': 'Annual COC Returns',
+            }}
+          />
+        </FormControl>
+      </div>
     </div>
   );
 }
